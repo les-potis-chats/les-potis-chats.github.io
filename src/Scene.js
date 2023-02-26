4794e4bc58764f937032c0a3ab117e9c105f517c
loicqql <loic.qql@gmail.com>
2023-02-26 20:54:20 +0100

    end

--- src/Scene.js
+++ src/Scene.js
@@ -8,8 +8,8 @@ import {
 } from "@react-three/drei";
 import { Suspense, useEffect, useRef, useState, useMemo } from "react";
 import { Car } from "./mouse/Car";
-import CatUp from "./objs/CatUp";
-import CatDown from "./objs/CatDown";
+import { CatUp } from "./objs/CatUp";
+import { CatDown } from "./objs/CatDown";
 import { Objs } from "./objs/Objs";
 import { Ground } from "./map/Ground";
 import { Bounds } from "./map/Bounds";
@@ -34,7 +34,7 @@ import { Pot4 } from "./objs/Pot4";
 
 export function Scene() {
 
-  const fontProps = { font: '/fonts/Inter-Medium.ttf', fontSize: 0.5 }
+  const fontProps = { font: '/fonts/PaytoneOne-Regular.ttf', fontSize: 0.5 }
 
   const [objs, setObjs] = useImmer([]);
   const [timer, setTimer] = useImmer(-1);
@@ -173,7 +173,8 @@ export function Scene() {
       if(catFollow) {
         let angle = Math.atan(StateManager.carPosition.x / StateManager.carPosition.z);
         angle += StateManager.carPosition.z > 0 ? Math.PI : 0;
-        catRef.current.rotation.y = angle
+        angle -= Math.PI / 2;
+        catRef.current.rotation.y = angle; 
       }else {
         catRef.current.rotation.y = Math.PI / 2;
       }
@@ -186,7 +187,7 @@ export function Scene() {
       {/* <OrbitControls position={[-2.34, 3.15, -4.07]} enablePan={false} /> */}
       <OrbitControls position={[-2.34, 3.15, -4.07]} />
       {/* <PerspectiveCamera makeDefault position={cameraPosition} fov={40} /> */}
-      <Stats />
+      {/* <Stats /> */}
       {/* <axesHelper args={[0.5]} position={[0, 0.5, 0]} /> */}
 
       {/* <arrowHelper position={[0, 0.1, -5]} ref={ref} /> */}
@@ -205,8 +206,10 @@ export function Scene() {
 
 
       {/* cat */}
-      <CatUp visible={!catDown} position={[0, 0.7, 0]} rotation-y={catR} scale={0.3} />
-      <CatDown visible={catDown} ref={catRef} position={[0, 0.7, 0]} rotation-y={catR} scale={0.3} />
+      <mesh ref={catRef}>
+        <CatUp visible={!catDown} position={[0, 0.7, 0]} rotation-y={catR} scale={0.3} />
+        <CatDown visible={catDown} position={[0, 0.7, 0]} rotation-y={catR} scale={0.3} />
+      </mesh>
 
       {/* <Cheese castShadow receiveShadow position={[0, 0, 5]} />
       <Portal castShadow receiveShadow position={[0, 0, 5]} /> */}
@@ -235,16 +238,16 @@ export function Scene() {
       <Ball position={objsManager.randomBall()} />
 
       {(timer > -1) && <>
-        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={Math.PI / 4} position={[0.52, 0.25, 0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
-        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={5 * Math.PI / 4} position={[-0.52, 0.25, -0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
-        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={3 * Math.PI / 4} position={[0.52, 0.25, -0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
-        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={7 * Math.PI / 4} position={[-0.52, 0.25, 0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
+        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={Math.PI / 4} position={[0.52, 0.3, 0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
+        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={5 * Math.PI / 4} position={[-0.52, 0.3, -0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
+        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={3 * Math.PI / 4} position={[0.52, 0.3, -0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
+        <Text color={timer > 5 ? 'black' : 'red'} rotation-y={7 * Math.PI / 4} position={[-0.52, 0.3, 0.52]} anchorX="center" anchorY="middle" {...fontProps}>{timer}</Text>
       </> }
 
       {(text !== '') && 
         <mesh ref={textRef} onClick={(e) => handleClick()}>
           <Center position={[0, 2, 0]}>
-            <Text3D position={[0, 2, 0]} scale={0.5} rotation={[0.1, 0, 0]} font={'/fonts/helvetiker_regular.typeface.json'} bevelEnabled bevelSize={0.05}>
+            <Text3D position={[0, 2, 0]} scale={0.5} rotation={[0.1, 0, 0]} font={'/fonts/Paytone One_Regular.json'} bevelEnabled bevelSize={0.05}>
               {text}
               <meshNormalMaterial />
             </Text3D>
