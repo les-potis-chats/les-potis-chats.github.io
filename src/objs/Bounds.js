61431e08cd7aca5c8c1df26d87e97529c62932b8
Greg0s <gregoire.tinnes@outlook.fr>
2023-02-26 15:52:22 +0100

    add bounds

---
+++ src/objs/Bounds.js
@@ -0,0 +1,38 @@
+import { ColliderBox } from "./ColliderBox";
+import { useEffect } from "react";
+import { useImmer } from "use-immer";
+
+export function Bounds() {
+  const [border, setBorder] = useImmer([]);
+
+  useEffect(() => {
+
+    let pos = [];
+    let index = 0;
+
+    for (let i = 0; i < 2 * Math.PI; i += Math.PI / 16) {
+      const angle = i;
+      let radius =  6;
+      let opposite = Math.cos(angle) * radius;
+      let adjacent = Math.sin(angle) * radius;
+      pos.push({
+        x: Math.cos(angle) * radius,
+        y: Math.sin(angle) * radius,
+        rot : Math.atan(opposite / adjacent),
+      })    
+    }
+
+    setBorder(pos);
+  }, [])
+
+  return <>
+
+    {/* Border */}
+    
+    {border.map((box, index) =>
+      <mesh key={index}>
+        <ColliderBox position={[box.x, 0.5, box.y]} scale={[1, 1, 1]} dpos={[0.0, 0.0, 0.0]} rotation={[0.0, box.rot, 0.0]}/>
+      </mesh>
+    )}
+  </>
+}
--- src/objs/ColliderBox.js
+++ src/objs/ColliderBox.js
@@ -2,16 +2,17 @@ import { useBox } from "@react-three/cannon";
 
 const debug = true;
 
-export function ColliderBox({ position, scale, dpos }) {
+export function ColliderBox({ position, scale, dpos, rotation }) {
   useBox(() => ({
     args: scale,
-    position,
+    position : [position[0] + dpos[0], position[1] + dpos[1], position[2] + dpos[2]],
     type: "Static",
+    rotation,
   }));
 
   return (
     debug && (
-      <mesh position={[position[0] + dpos[0], position[1] + dpos[1], position[2] + dpos[2]]}>
+      <mesh rotation={rotation} position={[position[0] + dpos[0], position[1] + dpos[1], position[2] + dpos[2]]}>
         <boxGeometry args={scale} />
         <meshBasicMaterial transparent={true} opacity={0.25} />
       </mesh>
